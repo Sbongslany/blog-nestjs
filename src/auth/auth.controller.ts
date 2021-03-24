12332322b6ef0177aaebcf2665ecd92432a17b29
sibongiseni <sibongiseni.ndebele@idolconsulting.co.za>
2021-03-24 20:13:08 +0200

    first-commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDTO, RegistrationDTO } from '../models/user.dto';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('/users')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post()
+  register(@Body(ValidationPipe) credentials: RegistrationDTO) {
+    return this.authService.register(credentials);
+  }
+
+  @Post('/login')
+  login(@Body(ValidationPipe) credentials: LoginDTO) {
+    return this.authService.login(credentials);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { LoginDTO, RegistrationDTO } from '../models/user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from '../entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UserEntity) private useRepo: Repository<UserEntity>,
+  ) {}
+
+  async register(credentials: RegistrationDTO) {
+    try {
+      const user = this.useRepo.create(credentials);
+      await user.save();
+      return user;
+    } catch (e) {
+      if (e.code === '23505') {
+        throw new ConflictException('User has already been taken');
+      }
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async login({ email, password }: LoginDTO) {
+    try {
+      const user = await this.useRepo.findOne({ where: { email } });
+      const isValid = await user.comparePassword(password);
+      if (!isValid) {
+        throw new UnauthorizedException('Invalid credentials');
+      }
+      return user;
+      
+    } catch (e) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+  }
+}
